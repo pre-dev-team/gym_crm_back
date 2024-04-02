19e026ea7e3ee8d3a37149fe75a043bf08ed406a
elikese <elikese@gamil.com>
2024-04-02 11:15:02 +0900

    init

---
+++ src/main/java/com/predev/gymcrm/GymCrmApplication.java
@@ -0,0 +1,13 @@
+package com.predev.gymcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymCrmApplication.class, args);
+	}
+
+}
