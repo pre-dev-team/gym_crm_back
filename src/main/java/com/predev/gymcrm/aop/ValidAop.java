0b30e4c00cbca2a29e1a2b26d261a8032595293a
leeseongchi <kyoti0414@naver.com>
2024-04-03 15:17:58 +0900

    Docs: 중간 작업 중

--- src/main/java/com/predev/gymcrm/aop/ValidAop.java
+++ src/main/java/com/predev/gymcrm/aop/ValidAop.java
@@ -1,2 +1,34 @@
-package com.predev.gymcrm.aop;public class ValidAop {
+package com.predev.gymcrm.aop;
+
+import com.predev.gymcrm.repository.UserMapper;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+
+@Aspect
+@Component
+public class ValidAop {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Pointcut("@annotaion(com.predev.gtmcrm.aop.annotation.ValidAspect)")
+    private void pointCut() {
+
+    }
+
+    @Around("pointCut()")
+    public Object around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        String methodName = proceedingJoinPoint.getSignature().getName();
+
+        Object[] args = proceedingJoinPoint.getArgs();
+
+        BeanPropertyBindingResult bindingResult = null;
+
+        return proceedingJoinPoint.proceed();
+    }
 }
