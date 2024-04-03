af49ed6eeadea81be7b30882707bed25d4d54f6a
leeseongchi <kyoti0414@naver.com>
2024-04-03 14:42:58 +0900

    Docs: 중간 작업 중

---
+++ src/main/java/com/predev/gymcrm/aop/annotation/ValidAspect.java
@@ -0,0 +1,8 @@
+package com.predev.gymcrm.aop.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidAspect {
+}
