8c9a512ccc16da9b4574202ec9d04c34ff99b763
elikese <elikese@gmail.com>
2024-04-03 21:55:40 +0900

    feat:security config ì변경  , refactor:jwt ì토큰°행을 위한 User Entity 변경

--- src/main/java/com/predev/gymcrm/config/SecurityConfig.java
+++ src/main/java/com/predev/gymcrm/config/SecurityConfig.java
@@ -1,17 +1,25 @@
 package com.predev.gymcrm.config;
 
+import com.predev.gymcrm.security.exception.AuthEntryPoint;
+import com.predev.gymcrm.security.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    JwtAuthenticationFilter jwtAuthenticationFilter;
 
+    @Autowired
+    AuthEntryPoint authEntryPoint;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -26,6 +34,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/user/**")
                 .hasRole("USER")
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(authEntryPoint);
     }
 }
