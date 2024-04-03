24485621f0b58fd55f8f7fe4a312a03e4fd57474
maybeags <suren0709@gmail.com>
2024-04-03 17:45:20 +0900

    Feat: 로그인 조회 및 기타

---
+++ src/main/java/com/predev/gymcrm/dto/req/UserSigninReqDto.java
@@ -0,0 +1,10 @@
+package com.predev.gymcrm.dto.req;
+
+import lombok.Data;
+
+@Data
+public class UserSigninReqDto {
+    private String userUsername;
+    private String userPassword;
+
+}
--- src/main/java/com/predev/gymcrm/service/AuthService.java
+++ src/main/java/com/predev/gymcrm/service/AuthService.java
@@ -1,10 +1,13 @@
 package com.predev.gymcrm.service;
 
 import com.predev.gymcrm.dto.req.TrainerSignupReqDto;
+import com.predev.gymcrm.dto.req.UserSigninReqDto;
+import com.predev.gymcrm.entity.User;
 import com.predev.gymcrm.repository.TrainerMapper;
 import com.predev.gymcrm.dto.req.UserSignupReqDto;
 import com.predev.gymcrm.repository.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,10 +18,23 @@ public class AuthService {
 
     @Autowired
     private UserMapper userMapper;
+
+    public boolean isDuplicatedByUsername(String userUsername) {
+        return userMapper.findUserByUsername(userUsername) != null;
+    }
     public void userSignup(UserSignupReqDto userSignupReqDto) {
         userMapper.saveUser(userSignupReqDto.toEntity());
     }
     public void trainerSignup(TrainerSignupReqDto trainerSignupReqDto) {
         trainerMapper.saveTrainer(trainerSignupReqDto.toEntity());
+    }
+
+    public String userSignin(UserSigninReqDto userSigninReqDto) {
+        User user = userMapper.findUserByUsername(userSigninReqDto.getUserUsername());
+        if(user == null ) {
+            throw new UsernameNotFoundException("사용자 정보를 확인하세요.");
+        }
+
+        return null;
     }
 }
