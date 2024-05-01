11ff126aacdf147af29f2813191c037ab945e974
elikese <elikese@gmail.com>
2024-05-02 05:58:47 +0900

    feat: firebase-admin 의존성 추가 , feat: FCMConfig 설정 구현

---
+++ src/main/java/com/predev/gymcrm/config/FCMConfig.java
@@ -0,0 +1,44 @@
+package com.study.library.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+@Configuration
+public class FCMConfig {
+    @Value("${fcm.certification}")
+    private String FIREBASE_CONFIG_PATH;
+
+    @PostConstruct
+    public FirebaseApp fcmInitialize() throws IOException {
+        try {
+            GoogleCredentials googleCredentials = GoogleCredentials.fromStream(new ClassPathResource(FIREBASE_CONFIG_PATH).getInputStream());
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(googleCredentials)
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp firebaseApp = FirebaseApp.initializeApp(options);
+                System.out.println("파이어베이스 앱 시작");
+                return firebaseApp;
+            } else {
+                return FirebaseApp.getInstance(); // 이미 초기화된 경우 기존 인스턴스 반환
+            }
+        } catch (IOException e) {
+            System.out.println("파이어베이스 앱 시작 실패: " + e.getMessage());
+            throw new IllegalStateException("파이어베이스 앱 시작 실패", e);
+        }
+    }
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging() {
+        return FirebaseMessaging.getInstance(FirebaseApp.getInstance());
+    }
+}
--- src/main/java/com/predev/gymcrm/controller/FCMPushNotificationController.java
+++ src/main/java/com/predev/gymcrm/controller/FCMPushNotificationController.java
@@ -2,6 +2,7 @@ package com.predev.gymcrm.controller;
 
 import com.predev.gymcrm.dto.req.FCMTokenReqDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/notification")
 public class FCMPushNotificationController {
 
+    @PostMapping("/register")
     public ResponseEntity<?> register(FCMTokenReqDto tokenReqDto) {
         return ResponseEntity.ok(tokenReqDto);
     }
