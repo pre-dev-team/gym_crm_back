6f09f60525171e0248e6bfaea298dff6cdf19e05
elikese <elikese@gmail.com>
2024-05-02 05:41:38 +0900

    feat: FCMPushNotificationController 생성

---
+++ src/main/java/com/predev/gymcrm/controller/FCMPushNotificationController.java
@@ -0,0 +1,15 @@
+package com.predev.gymcrm.controller;
+
+import com.predev.gymcrm.dto.req.FCMTokenReqDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notification")
+public class FCMPushNotificationController {
+
+    public ResponseEntity<?> register(FCMTokenReqDto tokenReqDto) {
+        return ResponseEntity.ok(tokenReqDto);
+    }
+}
